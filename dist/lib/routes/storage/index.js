d08a2bfcb7ac446c9d906d9e6ab0325331c7e9b3
anshinx <72576177+anshinx@users.noreply.github.com>
2023-03-14 15:51:35 +0300

    Delete dist directory

--- dist/lib/routes/storage/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dbRoute = (0, express_1.Router)();
-dbRoute.get("/", (request, response) => {
-    response.send("Reached");
-});
-exports.default = dbRoute;
